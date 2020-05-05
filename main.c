dc2f6ceda7ff035e6431e2b8e3a39ed3fb47953d
Luís Fernandes <luis.paulo.6.1.2001@gmail.com>
2020-05-05 14:02:10 +0100

    V1.1.2

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "gestaoDados.h"
 
 
@@ -23,24 +21,44 @@ int menu_arranque () {
     return opc;
 }
 int menu_user() {
-    int opc=0;
+    int opc = 0;
     printf("*******MENU*************\n");
     printf("******1-ACESSAR CONTA***\n");
     printf("******2-REGISTAR*******\n");
     printf("******0-SAIR***********\n");
     scanf("%i", &opc);
     return opc;
-
+}
 int main() {
     int nPerguntas=0;
+    int opc1=0, opc2=0;
+    USER newUser;
+    ELEMENTO *iniListaUser=NULL, *fimListaUser=NULL;
+
+    iniListaUser=(ELEMENTO *)calloc(1,sizeof(ELEMENTO));
 
     printf("\n");
     printf("Qual a quantidade de perguntas a jogar:");
     scanf("%i",&nPerguntas);
-    while(nPerguntas!=0){
 
-        nPerguntas--;
+    opc1=menu_arranque();
+    switch(opc1){
+        case 1:{
+            opc2=menu_user();
+            switch (opc2) {
+                case 1:{
+                    break;
+                }
+                case 2:{
+                    newUser=registarUser();
+                    inserirFimLista(iniListaUser,fimListaUser,newUser);
+                    break;
+                }
+
+            }
+        }
     }
+
 
     return 0;
 }
